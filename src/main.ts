eca75d1fba640c0740003cb5d30342a85ab6100c
Larissa <larissanunesvaz23@gmail.com>
2025-10-23 22:12:38 -0300

    ajustar para publicar

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Habilita CORS
+  app.enableCors({
+    origin: '*', 
+  });
+
+  // Usa a porta do Render ou 3000 como fallback
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`🚀 App running on port ${port}`);
 }
+
 bootstrap();
