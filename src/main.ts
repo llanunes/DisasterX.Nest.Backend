ecc323b790db399f565d5dba4a9f60d206bde519
Larissa <larissanunesvaz23@gmail.com>
2025-10-23 22:20:40 -0300

    ajustes

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilita CORS
+  // Habilita CORS para o frontend
   app.enableCors({
     origin: '*', 
   });
 
-  // Usa a porta do Render ou 3000 como fallback
+  // Porta fornecida pelo Render ou fallback 3000
   const port = process.env.PORT || 3000;
   await app.listen(port);
-
   console.log(`🚀 App running on port ${port}`);
 }
 
